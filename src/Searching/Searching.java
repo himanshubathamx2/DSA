352b2ecc2af0c2eae96a2d3f0949b99767b48c33
himanshubathamx2 <112793814+himanshubathamx2@users.noreply.github.com>
2023-08-31 03:16:53 +0530

    sortSearch

---
+++ src/Searching/Searching.java
@@ -0,0 +1,29 @@
+package Searching;
+public class Searching {
+
+  public static int linearSearch(int arr[], int value) {
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] == value) {
+        System.out.println("The element is found at the index: " + i);
+        return i;
+      }
+    }
+    System.out.println("The element " + value+ " not found.");
+    return -1;
+  }
+
+  // Binary Search
+
+  public static int binarySearch(int arr[], int value) {
+	  
+	  return 0;
+    }
+    
+
+
+ 
+
+
+  
+}
+
