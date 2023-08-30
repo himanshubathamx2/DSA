1a766bc6d5721a6c6b4ec0b3bdb73da662e2e25d
himanshubathamx2 <112793814+himanshubathamx2@users.noreply.github.com>
2023-08-30 15:37:50 +0530

    addinghash

---
+++ src/Hashing/DirectChaining/Main.java
@@ -0,0 +1,15 @@
+package Hashing.DirectChaining;
+
+class Main {
+  public static void main(String[] args) {
+    DirectChaining directChaining = new DirectChaining(13);
+    directChaining.insertHashTable("The");
+    directChaining.insertHashTable("quick");
+    directChaining.insertHashTable("brown");
+    directChaining.insertHashTable("fox");
+    directChaining.insertHashTable("over");
+	  directChaining.deleteKeyHashTable("ox");
+    directChaining.displayHashTable();
+
+  }
+}
