d7f08415345f514db3affb2ff07610485b4d3241
himanshubathamx2 <112793814+himanshubathamx2@users.noreply.github.com>
2023-09-02 16:13:20 +0530

    aaruKsathCommit

---
+++ src/DisjointSet/Cl.java
@@ -0,0 +1,12 @@
+package DisjointSet;
+
+import java.util.ArrayList;
+
+public class Cl
+{
+	public static void main(String[] args)
+	{
+		ArrayList<WeightedNode> nodeList = new ArrayList<>();
+		nodeList.add(new WeightedNode("A", 0));
+	}
+}
